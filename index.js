cebc566749b85c513ced78f1adccbb09e366c591
tubez7 <thescopeboys@hotmail.com>
2023-02-15 22:25:05 +0000

    complete rough build of homepage and general site structure

--- index.js
+++ index.js
@@ -2,24 +2,39 @@ const sideNav = document.getElementById("mySidenav");
 const main = document.getElementById("main");
 const hamburger = document.getElementById("hamburger");
 const filter = document.getElementById("opaque-filter");
+const projectList = document.getElementById("project-list");
+const expandProject = document.getElementById("projectExpandButton");
 
 function openNav() {
   sideNav.style.width = "250px";
-  //   main.style["padding-left"] = "250px";
   main.style.marginLeft = "250px";
-//   main.style.transition = "padding-left 0.5s";
   main.style.transition = "margin-left 0.5s";
+  // main.forEach((elem) => {
+  //   elem.style.marginLeft = "250px";
+  //   elem.style.transition = "margin-left 0.5s";
+  // });
   hamburger.style.visibility = "hidden";
   filter.style.visibility = "visible";
-
 }
 
 function closeNav() {
   sideNav.style.width = "0";
-//   main.style["padding-left"] = "0px";
   main.style.marginLeft = "0px";
-//   main.style.transition = "padding-left 0.5s";
   main.style.transition = "margin-left 0.5s";
+  // main.forEach((elem) => {
+  //   elem.style.marginLeft = "0px";
+  //   elem.style.transition = "margin-left 0.5s";
+  // });
   hamburger.style.visibility = "visible";
   filter.style.visibility = "hidden";
+}
+
+function displayProjects() {
+  if (projectList.style.display !== "none") {
+    projectList.style.display = "none";
+    expandProject.innerHTML = "Click to see course project details...";
+  } else {
+    projectList.style.display = "block";
+    expandProject.innerHTML = "Click to minimise...";
+  }
 }
